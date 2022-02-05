b75910ff525b3090a4558458538a0d05861fd11f
jariyawa <jariyavadee.26@mail.kmutt.ac.th>
2022-02-05 23:04:40 +0700

    commit

---
+++ basic-templete/basic-reactive/src/App-v-on.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+const base = 6
+const height = 6
+const result = ref(0)
+//arrow function expression
+const computeArea = () => {
+  result.value = (1 / 2) * base * height
+  console.log(result.value)
+}
+</script>
+
+<template>
+  <div>
+    <p>Base: {{ base }}</p>
+    <p>Height: {{ height }}</p>
+    <button @click="computeArea">Compute Area</button>
+    <p>Result:{{ result }}</p>
+  </div>
+</template>
---
+++ basic-templete/basic-reactive/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, computed } from 'vue'
+const myNotes = ref([])
+const newNote = ref('')
+const addNote = () => myNotes.value.push(newNote.value)
+</script>
+
+
+
+<template>
+    <div v-show="myNotes != ''">
+        Filter Note:
+        <input type="text" />
+        <ul>
+            <li v-for="(myNote, index) in myNotes" :key="index">{{ myNote }}</li>
+        </ul>
+    </div>New Note:
+    <input type="text" class="border-2 " v-model="newNote" />
+
+    <button @click="addNote" class="border-2 cursor-pointer ">Add Note</button>
+</template>
+
+
+
+<style></style>
